fb02b857204fbb4425d273e7b59465a7930afab6
Sultonazar Mamadazizov <sultonazar@outlook.com>
2019-08-30 22:16:24 +0300

    Bootstrap Vue

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,9 +5,10 @@
  */
 
 require('./bootstrap');
+let BootstrapVue = require('bootstrap-vue').default;
 
 window.Vue = require('vue');
-
+window.Vue.use(BootstrapVue);
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
