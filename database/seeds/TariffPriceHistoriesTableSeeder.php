a5743c3f42b4c5efe047362195ce75f0f9c7b0d5
Sultonazar Mamadazizov <sultonazar@outlook.com>
2019-09-07 19:42:12 +0300

    TariffPriceHistoriesTableSeeder was extened

--- database/seeds/TariffPriceHistoriesTableSeeder.php
+++ database/seeds/TariffPriceHistoriesTableSeeder.php
@@ -1,4 +1,4 @@
-<?php
+<?php /** @noinspection ALL */
 
 use App\Branch;
 use App\Tariff;
@@ -7,6 +7,8 @@ use Illuminate\Database\Seeder;
 
 class TariffPriceHistoriesTableSeeder extends Seeder
 {
+    private $tariffs = array();
+
     /**
      * Run the database seeds.
      *
@@ -14,6 +16,9 @@ class TariffPriceHistoriesTableSeeder extends Seeder
      */
     public function run()
     {
+        $branchId = Branch::first()->id;
+        $this->tariffs = Tariff::all();
+
         $newPrice = new TariffPriceHistory();
         $newPrice->lowerLimit = 110;
         $newPrice->mediumLimit = 170;
@@ -26,10 +31,84 @@ class TariffPriceHistoriesTableSeeder extends Seeder
         $newPrice->maxWeightPerCube = 250;
         $newPrice->maxCubage = 108;
         $newPrice->maxWeight = 27000;
-        $newPrice->branch_id = Branch::first()->id;
-        $newPrice->tariff_id = Tariff::first()->id;
+        $newPrice->branch_id = $branchId;
+        $newPrice->tariff_id = $this->getId('Обувь-У');
         $newPrice->save();
+
+        $tariff=$this->getId('Пресс-У');
+        TariffPriceHistory::create([
+            'lowerLimit'=>110,
+            'mediumLimit'=>170,
+            'upperLimit'=>200,
+            'discountForLowerLimit'=>0,
+            'discountForMediumLimit'=>0,
+            'pricePerCube'=>160,
+            'agreedPricePerKg'=>0.7,
+            'pricePerExtraKg'=>0.33,
+            'maxWeightPerCube'=>250,
+            'maxCubage'=>108,
+            'maxWeight'=>27000,
+            'branch_id'=>$branchId,
+            'tariff_id'=>$tariff
+        ]);
+
+        $tariff=$this->getId('Запчасти-У');
+        TariffPriceHistory::create([
+            'lowerLimit'=>110,
+            'mediumLimit'=>170,
+            'upperLimit'=>200,
+            'discountForLowerLimit'=>10,
+            'discountForMediumLimit'=>5,
+            'pricePerCube'=>175,
+            'agreedPricePerKg'=>0.77,
+            'pricePerExtraKg'=>0.35,
+            'maxWeightPerCube'=>250,
+            'maxCubage'=>108,
+            'maxWeight'=>27000,
+            'branch_id'=>$branchId,
+            'tariff_id'=>$tariff
+        ]);
+
+        $tariff=$this->getId('Договорной-У');
+        TariffPriceHistory::create([
+            'lowerLimit'=>110,
+            'mediumLimit'=>170,
+            'upperLimit'=>200,
+            'discountForLowerLimit'=>10,
+            'discountForMediumLimit'=>5,
+            'pricePerCube'=>170,
+            'agreedPricePerKg'=>0.67,
+            'pricePerExtraKg'=>0.33,
+            'maxWeightPerCube'=>250,
+            'maxCubage'=>108,
+            'maxWeight'=>27000,
+            'branch_id'=>$branchId,
+            'tariff_id'=>$tariff
+        ]);
+
+        $tariff=$this->getId('Кашкар');
+        TariffPriceHistory::create([
+            'lowerLimit'=>110,
+            'mediumLimit'=>170,
+            'upperLimit'=>200,
+            'discountForLowerLimit'=>10,
+            'discountForMediumLimit'=>5,
+            'pricePerCube'=>155,
+            'agreedPricePerKg'=>0.6,
+            'pricePerExtraKg'=>0.33,
+            'maxWeightPerCube'=>250,
+            'maxCubage'=>108,
+            'maxWeight'=>27000,
+            'branch_id'=>$branchId,
+            'tariff_id'=>$tariff
+        ]);
     }
 
-
+    private function getId($tariffName){
+      foreach ($this->tariffs as $tariff){
+          if($tariff->name === $tariffName)
+              return $tariff->id;
+      }
+       return null;
+    }
 }
