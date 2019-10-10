c67617057db4f28fcc429946619481237746f4e7
Sultonazar Mamadazizov <sultonazar@outlook.com>
2019-10-10 11:43:15 +0300

    user register and edit views -> shadow added

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
-                <div class="card">
+                <div class="card shadow">
                     <div class="card-header">Редактирование профиля</div>
 
                     <div class="card-body">
