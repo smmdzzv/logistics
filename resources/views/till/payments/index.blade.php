365b6c78f9f788c0f6349b77aef20401a1db470b
Sultonazar Mamadazizov <sultonazar@outlook.com>
2019-10-16 00:15:44 +0300

    balance info

--- resources/views/till/payments/index.blade.php
+++ resources/views/till/payments/index.blade.php
@@ -2,10 +2,11 @@
 
 @section('content')
     <div class="container">
-        <div class="row ">
-            <div class="col-md-10  text-right">Баланс: {{$account->balance}}</div>
+        <div class="row py-3">
+            <div class="text-right pl-5"> {{$account->description}} | Баланс: {{$account->balance}} {{$account->currency->isoName}}</div>
         </div>
         <div class="row justify-content-center">
+
             <payments-table :branches="{{$branches}}"></payments-table>
         </div>
     </div>
