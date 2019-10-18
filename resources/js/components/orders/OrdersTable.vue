f6e87dcccaf41a44f2a15f7fa380f4a69bb814e1
Sultonazar Mamadazizov <sultonazar@outlook.com>
2019-10-19 00:18:18 +0300

    Orders tbale to excel

--- resources/js/components/orders/OrdersTable.vue
+++ resources/js/components/orders/OrdersTable.vue
@@ -1,71 +1,72 @@
 <template>
-        <div class="card shadow">
-            <div class="card-header">
-                <div class="row align-items-baseline">
-                    <div class="col-6 col-md-4">Заказы</div>
-                    <template  v-if="branches" >
-                        <label class="col-6 col-md-4 text-right" for="branch">Филиалы</label>
-                        <div class="col-md-4">
-                            <select id="branch" class="form-control custom-select" v-model="selectedBranch">
-                                <option v-for="branch in branches" :value="branch" :key="branch.id">{{branch.name}}</option>
-                            </select>
-                        </div>
-                    </template>
-                </div>
-            </div>
-            <b-table :fields="fields"
-                     id="usersTable"
-                     :items="orders"
-                     :busy="isBusy"
-                     striped
-                     borderless
-                     primary-key="id"
-                     responsive>
-                <template v-slot:table-busy>
-                    <div class="text-center text-info my-2">
-                        <b-spinner class="align-middle"></b-spinner>
+    <table-card
+        :customCells="customCells"
+        :excelSheetName="selectedBranch ? selectedBranch.name : 'Все филиалы'"
+        :fields="fields"
+        :isBusy="isBusy"
+        :items="orders"
+        striped
+        class="shadow"
+        excelFileName="Список заказов"
+        primary-key="id"
+        responsive>
+        <template #header>
+            <div class="row align-items-baseline">
+                <div class="col-6 col-md-4">Заказы</div>
+                <template v-if="branches">
+                    <label class="col-6 col-md-4 text-right" for="branch">Филиалы</label>
+                    <div class="col-md-4">
+                        <select class="form-control custom-select" id="branch" v-model="selectedBranch">
+                            <option :value="null">--Все филиалы--</option>
+                            <option :key="branch.id" :value="branch" v-for="branch in branches">{{branch.name}}</option>
+                        </select>
                     </div>
                 </template>
-
-                <template slot="details" slot-scope="{item}">
-                    <a class="btn btn-outline-primary" :href="getDetailsUrl(item)">Детали</a>
-                </template>
-            </b-table>
-            <div class="card-footer">
-<!--                <pagination :data="pagination" @pagination-change-page="getOrders"></pagination>-->
-                <main-paginator :pagination="pagination" :onPageChange="getOrders" :flowable="flowable"></main-paginator>
             </div>
-        </div>
+        </template>
+
+        <template slot="details" slot-scope="{item}">
+            <a :href="getDetailsUrl(item)" class="btn btn-outline-primary">Детали</a>
+        </template>
+
+        <template #footer>
+            <div class="card-footer">
+                <main-paginator :flowable="flowable" :onPageChange="getOrders"
+                                :pagination="pagination"></main-paginator>
+            </div>
+        </template>
+    </table-card>
 </template>
 
 <script>
     export default {
         name: "OrdersTable",
-        mounted(){
+        mounted() {
             this.getOrders();
         },
-        props:{
-            branches:{
-                type:Array,
+        props: {
+            branches: {
+                type: Array,
                 required: false,
             },
-            url:{
-                type:String,
-                default:'/orders/all'
+            url: {
+                type: String,
+                default: '/orders/all'
             },
-            flowable:{
-                type:Boolean,
-                default:false
+            flowable: {
+                type: Boolean,
+                default: false
             }
         },
-        methods:{
-            getOrders(page = 1){
+        methods: {
+            getOrders(page = 1) {
                 this.isBusy = true;
-                if(this.selectedBranch)
-                    this.action = `branch/${this.selectedBranch.id}/orders`;
-                this.action += '?paginate=7&page=' + page;
-                axios.get(this.action)
-                    .then(response=>{
+                let action = this.action;
+                if (this.selectedBranch)
+                    action = `branch/${this.selectedBranch.id}/orders`;
+                action += '?paginate=7&page=' + page;
+                axios.get(action)
+                    .then(response => {
                         this.pagination = response.data;
                         if (this.flowable)
                             response.data.data.forEach(item => {
@@ -78,16 +79,16 @@
                         })
                     });
             },
-            getDetailsUrl(order){
+            getDetailsUrl(order) {
                 return '/orders/' + order.id;
             }
         },
-        computed:{
-            currentPage () {
-                return  this.pagination.current_page;
+        computed: {
+            currentPage() {
+                return this.pagination.current_page;
             }
         },
-        watch:{
+        watch: {
             selectedBranch: function () {
                 this.getOrders();
             }
@@ -95,10 +96,11 @@
         data() {
             return {
                 selectedBranch: null,
-                pagination:{},
+                pagination: {},
                 orders: [],
                 action: this.url,
-                isBusy:false,
+                isBusy: false,
+                customCells: ['details'],
                 fields: {
                     totalWeight: {
                         label: 'Вес',
@@ -116,26 +118,27 @@
                         label: 'Цена',
                         sortable: true
                     },
-                    'registered_by.name':{
-                        label:'Принял',
+                    'registered_by.name': {
+                        label: 'Принял',
                         sortable: true
                     },
-                    'owner.name':{
-                        label:'Владелец',
+                    'owner.name': {
+                        label: 'Владелец',
                         sortable: true
                     },
-                    created_at:{
-                        label:'Дата',
+                    created_at: {
+                        label: 'Дата',
                         sortable: true
                     },
-                    'details':{
-                        label:'',
+                    'details': {
+                        label: '',
                     }
                 }
             }
         },
-        components:{
-            'MainPaginator': require('../common/MainPaginator.vue').default
+        components: {
+            'MainPaginator': require('../common/MainPaginator.vue').default,
+            'TableCard': require('../common/TableCard.vue').default
         }
     }
 </script>
