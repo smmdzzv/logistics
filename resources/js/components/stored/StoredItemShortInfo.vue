a70eb499ae078690bb84db6c111ae120ea06667c
Sultonazar Mamadazizov <sultonazar@outlook.com>
2019-10-22 19:37:08 +0300

    items label view was changed

--- resources/js/components/stored/StoredItemShortInfo.vue
+++ resources/js/components/stored/StoredItemShortInfo.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="container border-bottom pt-3">
+    <div class="container border-bottom" style="margin-bottom:25px; padding-bottom:8px">
         <div class="container">
             <p>{{`Тип: ${storedItemInfo.item.name}`}}</p>
             <p>{{`Код: ${storedItem.code}`}}</p>
@@ -8,8 +8,8 @@
             <p>{{`Принят: ${storedItem.created_at}`}} </p>
         </div>
 
-        <div style="width:100px">
-            <barcode :options='{displayValue:false, height:50, width:1}' :value="storedItem.code" tag="svg"></barcode>
+        <div>
+            <barcode :options='{displayValue:false, height:50, width:1.6}' :value="storedItem.code" tag="svg"></barcode>
         </div>
     </div>
 </template>
