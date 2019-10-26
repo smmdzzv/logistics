6592ea7117b3b2fbe7be9996a37803999f27dd86
Sultonazar Mamadazizov <sultonazar@outlook.com>
2019-10-27 01:36:46 +0300

    users table migration bug fixed for MariaDB

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,9 +22,10 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->string('api_token', 80)->after('password')
+            $table->string('api_token', 80)
                 ->unique()
-                ->nullable();
+                ->nullable()
+                ->default(null);
             $table->string('phone')->nullable();
             $table->rememberToken();
             $table->timestamps();
