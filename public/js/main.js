fcac1fcb24fcef1f7f6b7db3c3dd9b3d766ffee2
Sultonazar Mamadazizov <sultonazar@outlook.com>
2019-10-27 22:52:03 +0300

    change focus on enter

--- public/js/main.js
+++ public/js/main.js
@@ -35,3 +35,21 @@ async function getOrderItems(orderId) {
     let action = `/order/${orderId}/items`;
     return axios.get(action);
 }
+
+// register jQuery extension
+jQuery.extend(jQuery.expr[':'], {
+    focusable: function (el, index, selector) {
+        return $(el).is('a, button, :input, [tabindex]');
+    }
+});
+
+$(document).on('keypress', 'input,select', function (e) {
+    if (e.which == 13) {
+        e.preventDefault();
+        // Get all focusable elements on the page
+        var $canfocus = $(':focusable');
+        var index = $canfocus.index(this) + 1;
+        if (index >= $canfocus.length) index = 0;
+        $canfocus.eq(index).focus();
+    }
+});
