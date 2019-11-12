0589adf3414b86f11189f3c8e123b58cd6df4230
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2019-11-12 14:45:17 +0300

    generate trip items list draft5

--- app/Data/Helpers/GenerateTripStoredItemListHelper.php
+++ app/Data/Helpers/GenerateTripStoredItemListHelper.php
@@ -6,6 +6,7 @@ namespace App\Data\Helpers;
 
 use App\Models\StoredItems\StoredItem;
 use App\Models\Trip;
+use Illuminate\Support\Collection;
 
 class GenerateTripStoredItemListHelper
 {
@@ -53,6 +54,7 @@ class GenerateTripStoredItemListHelper
         $this->storedItems = $this->storedItems
             ->groupBy(['info.ownerId', 'info.id'])
             ->flatten(2)
+            ->sortByDesc('info.weight')
             ->values();
 //        dd($this->storedItems->toArray());
     }
@@ -92,35 +94,35 @@ class GenerateTripStoredItemListHelper
 
     public function generate()
     {
-
+//        dd($this->storedItems->toArray());
         $options = $this->calculatePossibleOptions();
-
+        return $options;
 //        $maxWeights = $this->extractOptionsMaxWeights($options);
 
-        $index = count($options);
+//        $index = count($options);
 
-        $optimalTripItemsArray = [];
+//        $optimalTripItemsArray = [];
+//
+//        while ($index) {
+//
+//            if (count($options[--$index]) > 1) {
+//
+//                $optimalTripItemsArray = array_map(function ($item) {
+//
+//                    return isset($item['id']) ? $item['id'] : null;
+//
+//                }, $options[--$index]);
+//
+//                break;
+//            }
+//
+//        }
+//
+//        $optimalTripItemsArray = array_filter($optimalTripItemsArray, function ($item) {
+//            return $item !== null;
+//        });
 
-        while ($index) {
-
-            if (count($options[--$index]) > 1) {
-
-                $optimalTripItemsArray = array_map(function ($item) {
-
-                    return isset($item['id']) ? $item['id'] : null;
-
-                }, $options[--$index]);
-
-                break;
-            }
-
-        }
-
-        $optimalTripItemsArray = array_filter($optimalTripItemsArray, function ($item) {
-            return $item !== null;
-        });
-
-        return $optimalTripItemsArray;
+//        return $optimalTripItemsArray;
     }
 
 //    private function extractOptionsMaxWeights($options){
@@ -133,63 +135,96 @@ class GenerateTripStoredItemListHelper
 //        return $maxArr;
 //    }
 
+    private function calculatePossibleOptions()
+    {
+        $n = count($this->storedItems);
+        
+        $result = new Collection();
+
+        $maxC = $this->maxCubage;
+        $maxW = $this->maxWeight;
+
+        for ($i = 0; $i < $n; $i++) {
+            $stored = $this->storedItems[$i];
+
+            $cubage = $stored->info->cubage;
+            $weight = $stored->info->weight;
+
+            if ($maxW - $weight > 0) {
+                if ($maxC - $cubage > 0) {
+                    $result->push($stored);
+                    $maxC -= $cubage;
+                    $maxW -= $weight;
+                }
+            }
+        }
+
+
+        $result = $result->map(function ($item){
+            return $item->id;
+        });
+
+        return $result->toArray();
+
+    }
+
     /**
      * @return array
      */
-    private function calculatePossibleOptions()
-    {
-
-        $n = count($this->storedItems);
-        $C = $this->maxCubage;
-        $d = [0 => array_fill(0, $C + 1, ['weight' => 0])];
-
-        for ($i = 1; $i <= $n; $i++) {
-            $achieved = false;
-
-            for ($j = 0; $j <= $C; $j++) {
-
-                if ($j > 0 && isset($d[$i - 1][$j]['id']) && isset($d[$i - 1][$j - 1]['id'])
-                    && $d[$i - 1][$j - 1]['id'] === $d[$i - 1][$j]['id']
-                    && $d[$i][$j - 1]['id'] === $this->storedItems[$i - 1]->id){
-                    $d[$i][$j] = $d[$i][$j - 1];
-                    continue;
-                }
-
-
-                    $d[$i][$j] = $d[$i - 1][$j];
-
-                $wi = $this->storedItems[$i - 1]->info->weight;
-                $ci = $this->storedItems[$i - 1]->info->cubage;
-
-
-                if ($j - $ci >= 0 && $d[$i][$j]['weight'] < $d[$i - 1][$j - $ci]['weight'] + $wi) {
-
-                    $newWeight = $d[$i - 1][$j - $ci]['weight'] + $wi;
-
-                    if ($newWeight > $this->maxWeight) {
-                        array_splice($d[$i], $j, 1);
-                        $achieved = true;
-                        break;
-                    }
-
-                    $d[$i][$j]['weight'] = $newWeight;
-                    $d[$i][$j]['id'] = $this->storedItems[$i - 1]->id;
-                }
-            }
-            if ($achieved)
-                break;
-        }
-//        dd($d[100]);
-        return $d;
-//        $max = [];
+//    private function calculatePossibleOptions2()
+//    {
 //
-//        foreach ($d as $item) {
-//            $max[] = max($item);
+//        $n = count($this->storedItems);
+//        $C = $this->maxCubage;
+//        $d = [0 => array_fill(0, $C + 1, ['weight' => 0])];
+//
+//        for ($i = 1; $i <= $n; $i++) {
+//            $achieved = false;
+//
+//            for ($j = 0; $j <= $C; $j++) {
+//
+//                if ($j > 0 && isset($d[$i - 1][$j]['id']) && isset($d[$i - 1][$j - 1]['id'])
+//                    && $d[$i - 1][$j - 1]['id'] === $d[$i - 1][$j]['id']
+//                    && $d[$i][$j - 1]['id'] === $this->storedItems[$i - 1]->id) {
+//                    $d[$i][$j] = $d[$i][$j - 1];
+//                    continue;
+//                }
+//
+//
+//                $d[$i][$j] = $d[$i - 1][$j];
+//
+//                $wi = $this->storedItems[$i - 1]->info->weight;
+//                $ci = $this->storedItems[$i - 1]->info->cubage;
+//
+//
+//                if ($j - $ci >= 0 && $d[$i][$j]['weight'] < $d[$i - 1][$j - $ci]['weight'] + $wi) {
+//
+//                    $newWeight = $d[$i - 1][$j - $ci]['weight'] + $wi;
+//
+//                    if ($newWeight > $this->maxWeight) {
+//                        array_splice($d[$i], $j, 1);
+//                        $achieved = true;
+//                        break;
+//                    }
+//
+//                    $d[$i][$j]['weight'] = $newWeight;
+//                    $d[$i][$j]['id'] = $this->storedItems[$i - 1]->id;
+//                }
+//            }
+//            if ($achieved)
+//                break;
 //        }
-//
-//
-//        dd($max, $d[count($d) - 1], $n, $this->maxCubage);
-    }
+////        dd($d[100]);
+//        return $d;
+////        $max = [];
+////
+////        foreach ($d as $item) {
+////            $max[] = max($item);
+////        }
+////
+////
+////        dd($max, $d[count($d) - 1], $n, $this->maxCubage);
+//    }
 
 //    public function generate()
 //    {
