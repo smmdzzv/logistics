fdf163c08cc2e78a72400c08f965fd756df26156
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-01-21 11:12:42 +0500

    Order Payment and Order Payment Items models

---
+++ database/migrations/2020_01_21_060746_create_order_payment_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderPaymentItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('order_payment_items', function (Blueprint $table) {
+            $table->char('id', 26)->primary();
+            $table->char('order_payment_id', 26)->index();
+            $table->char('stored_item_id', 26)->index();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('order_payment_items');
+    }
+}
