f8d08794c1e31536cc44e31cd678a30d825b7191
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-02-03 14:35:22 +0500

    Navigation restrictions

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -41,101 +41,124 @@
                 <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav ml-4 mr-auto">
                         {{--Orders--}}
-                        <li class="nav-item dropdown">
-                            <a id="ordersMenuDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                Заказы <span class="caret"></span>
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="ordersMenuDropdown">
-                                <a class="dropdown-item" href="{{ route('orders.create') }}">Оформить заказ</a>
-                                <a class="dropdown-item" href="{{route('orders.index')}}">Список заказов</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{route('stored.index')}}">Принятые товары</a>
-                                <a class="dropdown-item" href="{{route('order-items.edit')}}">Выдать товары</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{route('lost-items.index')}}">Потерянные товары</a>
-                                <div class="dropdown-divider"></div>
-                                <h6 class="dropdown-header">Тарифы и расценки</h6>
-                                <a class="dropdown-item" href="{{route('tariffs.index')}}">Управление тарифами</a>
-                                {{--                                <a class="dropdown-item" href="{{route('tariff-price-histories.create')}}">Обновить--}}
-                                {{--                                    расценки</a>--}}
-                                <a class="dropdown-item" href="{{route('tariff-price-histories.index')}}">История
-                                    цен</a>
-                                <div class="dropdown-divider"></div>
-                                <h6 class="dropdown-header">Наименования</h6>
-                                <a class="dropdown-item" href="{{route('items.create')}}">Добавить наименование</a>
-                                <a class="dropdown-item" href="{{route('items.index')}}">Список наименований</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{route('customs-code.index')}}">Таможенные коды</a>
-                                <a class="dropdown-item" href="{{route('shop.create')}}">Добавить магазин</a>
-                            </div>
-                        </li>
-                        <!--Till-->
-                        <li class="nav-item dropdown">
-                            <a id="tillMenuDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                Касса <span class="caret"></span>
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="tillMenuDropdown">
-                                <h6 class="dropdown-header">Провести платеж</h6>
-                                <a class="dropdown-item" href="{{route('incoming-payments.create')}}">Приход</a>
-                                <a class="dropdown-item" href="{{route('outgoing-payments.create')}}">Расход</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{route('money-exchange.exchanger')}}">Обмен валют</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{route('pending-payments.index')}}">Заявки</a>
-                                <a class="dropdown-item" href="{{route('payments.index')}}">История платежей</a>
-                                <div class="dropdown-divider"></div>
-                                <a class="dropdown-item" href="{{route('legal-entity.accounts.index')}}">Счета Дуоб</a>
-                                <div class="dropdown-divider"></div>
-                                <h6 class="dropdown-header">Статьи прихода/расхода</h6>
-                                <a class="dropdown-item" href="{{route('payment-items.create')}}">Добавить статью</a>
-                                <a class="dropdown-item" href="{{route('payment-items.index')}}">Список статей</a>
-                                <div class="dropdown-divider"></div>
-                                <h6 class="dropdown-header">Валюта</h6>
-                                <a class="dropdown-item" href="{{route('currencies.create')}}">Добавить валюту</a>
-                                <a class="dropdown-item" href="{{route('currencies.index')}}">Список валют</a>
-                            </div>
-                        </li>
-                        <!--Users-->
-                        <li class="nav-item dropdown">
-                            <a id="usersMenuDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                Пользователи <span class="caret"></span>
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="usersMenuDropdown">
-                                <a class="dropdown-item" href="{{route("trusted-user.index")}}">Доверенные клиенты</a>
-                                <a class="dropdown-item" href="{{route("users.create")}}">Регистрация</a>
-                                <a class="dropdown-item" href="{{route("users.index")}}">Список</a>
-                            </div>
-                        </li>
-                        <!--Trips-->
-                        <li class="nav-item dropdown">
-                            <a id="tripsMenuDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                Рейсы <span class="caret"></span>
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="tripsMenuDropdown">
-                                <a class="dropdown-item" href="{{route('trips.create')}}">Создать рейс</a>
-                                <a class="dropdown-item" href="{{route('trips.index')}}">Список рейсов</a>
-                                <div class="dropdown-divider"></div>
-                                <h6 class="dropdown-header">Машины</h6>
-                                <a class="dropdown-item" href="{{route('cars.create')}}">Добавить машину</a>
-                                <a class="dropdown-item" href="{{route('cars.index')}}">Список машин</a>
-                            </div>
-                        </li>
-                        <!--Branches-->
-                        <li class="nav-item dropdown">
-                            <a id="usersMenuDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                Филиалы <span class="caret"></span>
-                            </a>
-                            <div class="dropdown-menu" aria-labelledby="usersMenuDropdown">
-                                <a class="dropdown-item" href="{{route('branches.index')}}">Управлять</a>
-                            </div>
-                        </li>
+                        @if(Auth::user()->hasAnyRole(['admin', 'manager']))
+                            <li class="nav-item dropdown">
+                                <a id="ordersMenuDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Заказы <span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="ordersMenuDropdown">
+                                    <a class="dropdown-item" href="{{ route('orders.create') }}">Оформить заказ</a>
+                                    <a class="dropdown-item" href="{{route('orders.index')}}">Список заказов</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="{{route('stored.index')}}">Принятые товары</a>
+                                    <a class="dropdown-item" href="{{route('order-items.edit')}}">Выдать товары</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="{{route('lost-items.index')}}">Потерянные товары</a>
 
-                        <!--Storage-->
+                                    @if(Auth::user()->hasRole('admin'))
+                                        <div class="dropdown-divider"></div>
+                                        <h6 class="dropdown-header">Тарифы и расценки</h6>
+                                        <a class="dropdown-item" href="{{route('tariffs.index')}}">Управление
+                                            тарифами</a>
+                                        {{--                                <a class="dropdown-item" href="{{route('tariff-price-histories.create')}}">Обновить--}}
+                                        {{--                                    расценки</a>--}}
+                                        <a class="dropdown-item" href="{{route('tariff-price-histories.index')}}">История
+                                            цен</a>
+                                        <div class="dropdown-divider"></div>
+                                        <h6 class="dropdown-header">Наименования</h6>
+                                        <a class="dropdown-item" href="{{route('items.create')}}">Добавить
+                                            наименование</a>
+                                        <a class="dropdown-item" href="{{route('items.index')}}">Список наименований</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="{{route('customs-code.index')}}">Таможенные
+                                            коды</a>
+                                        {{--                                <a class="dropdown-item" href="{{route('shop.create')}}">Добавить магазин</a>--}}
+                                    @endif
+                                </div>
+                            </li>
+                        @endif
+                    <!--Till-->
+                        @if(Auth::user()->hasAnyRole(['admin', 'cashier']))
+                            <li class="nav-item dropdown">
+                                <a id="tillMenuDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Касса <span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="tillMenuDropdown">
+                                    <h6 class="dropdown-header">Провести платеж</h6>
+                                    <a class="dropdown-item" href="{{route('incoming-payments.create')}}">Приход</a>
+                                    <a class="dropdown-item" href="{{route('outgoing-payments.create')}}">Расход</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="{{route('money-exchange.exchanger')}}">Обмен
+                                        валют</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="{{route('pending-payments.index')}}">Заявки</a>
+                                    <a class="dropdown-item" href="{{route('payments.index')}}">История платежей</a>
+                                    <div class="dropdown-divider"></div>
+                                    <a class="dropdown-item" href="{{route('legal-entity.accounts.index')}}">Счета
+                                        Дуоб</a>
+                                    @if(Auth::user()->hasRole('admin'))
+                                        <div class="dropdown-divider"></div>
+                                        <h6 class="dropdown-header">Статьи прихода/расхода</h6>
+                                        <a class="dropdown-item" href="{{route('payment-items.create')}}">Добавить
+                                            статью</a>
+                                        <a class="dropdown-item" href="{{route('payment-items.index')}}">Список
+                                            статей</a>
+                                        <div class="dropdown-divider"></div>
+                                        <h6 class="dropdown-header">Валюта</h6>
+                                        <a class="dropdown-item" href="{{route('currencies.create')}}">Добавить
+                                            валюту</a>
+                                        <a class="dropdown-item" href="{{route('currencies.index')}}">Список валют</a>
+                                    @endif
+                                </div>
+                            </li>
+                        @endif
+                    <!--Users-->
+                        @if(Auth::user()->hasRole('admin'))
+                            <li class="nav-item dropdown">
+                                <a id="usersMenuDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Пользователи <span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="usersMenuDropdown">
+                                    <a class="dropdown-item" href="{{route("trusted-user.index")}}">Доверенные
+                                        клиенты</a>
+                                    <a class="dropdown-item" href="{{route("users.create")}}">Регистрация</a>
+                                    <a class="dropdown-item" href="{{route("users.index")}}">Список</a>
+                                </div>
+                            </li>
+                        @endif
+                    <!--Trips-->
+                        @if(Auth::user()->hasRole('admin'))
+                            <li class="nav-item dropdown">
+                                <a id="tripsMenuDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Рейсы <span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="tripsMenuDropdown">
+                                    <a class="dropdown-item" href="{{route('trips.create')}}">Создать рейс</a>
+                                    <a class="dropdown-item" href="{{route('trips.index')}}">Список рейсов</a>
+                                    <div class="dropdown-divider"></div>
+                                    <h6 class="dropdown-header">Машины</h6>
+                                    <a class="dropdown-item" href="{{route('cars.create')}}">Добавить машину</a>
+                                    <a class="dropdown-item" href="{{route('cars.index')}}">Список машин</a>
+                                </div>
+                            </li>
+                        @endif
+                    <!--Branches-->
+                        @if(Auth::user()->hasRole('admin'))
+                            <li class="nav-item dropdown">
+                                <a id="usersMenuDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    Филиалы <span class="caret"></span>
+                                </a>
+                                <div class="dropdown-menu" aria-labelledby="usersMenuDropdown">
+                                    <a class="dropdown-item" href="{{route('branches.index')}}">Управлять</a>
+                                </div>
+                            </li>
+                        @endif
+                    <!--Storage-->
                         {{--                        <li class="nav-item dropdown">--}}
                         {{--                            <a id="storageMenuDropdown" class="nav-link dropdown-toggle" href="#" role="button"--}}
                         {{--                               data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>--}}
@@ -192,13 +215,13 @@
         @yield('content')
     </main>
     <b-modal
-        no-close-on-esc
-        no-close-on-backdrop
-        hide-footer
-        hide-header
-        centered
-        content-class="bg-transparent border-0"
-        id="busyModal">
+            no-close-on-esc
+            no-close-on-backdrop
+            hide-footer
+            hide-header
+            centered
+            content-class="bg-transparent border-0"
+            id="busyModal">
         <div class="d-block text-center">
             <b-spinner variant="light" label="Busy" style="width: 6rem; height: 6rem"/>
         </div>
