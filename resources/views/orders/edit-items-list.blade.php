b54c5ef8e0181e79229486ec8d59a2cc8bbf6926
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-02-03 17:35:44 +0500

    edit items list -> minor changes

--- resources/views/orders/edit-items-list.blade.php
+++ resources/views/orders/edit-items-list.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-10">
+            <div class="col-xl-10">
                 <div class="card shadow">
                     <div class="card-header">Выдать товары клиенту</div>
                     <div class="card-body">
