0763f464af9126d39c620124d331391fd6b55560
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-02-12 15:13:11 +0500

    StoredItemsController -> index page update

--- resources/views/stored/index.blade.php
+++ resources/views/stored/index.blade.php
@@ -5,7 +5,7 @@
 {{--        <stored-table class="shadow" flowable excel-export :branches="{{$branches}}"></stored-table>--}}
 {{--    </div>--}}
     <div class="container col-12">
-        <stored-item-info-table class="shadow" flowable excel-export url="stored-item-info/filtered?" :branches="{{$branches}}"></stored-item-info-table>
+        <stored-item-info-table class="shadow" flowable excel-export url="stored-item-info/filtered?" :columns-to-hide="['selectedCount']" :branches="{{$branches}}"></stored-item-info-table>
     </div>
 @endsection
 
