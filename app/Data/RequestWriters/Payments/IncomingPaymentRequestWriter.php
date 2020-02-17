31144443290d91be1b96b4d0fdaff201fa4c0d13
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-02-17 13:31:38 +0500

    redundant  writers are commented

--- app/Data/RequestWriters/Payments/IncomingPaymentRequestWriter.php
+++ app/Data/RequestWriters/Payments/IncomingPaymentRequestWriter.php
@@ -10,69 +10,69 @@ use App\User;
 
 class IncomingPaymentRequestWriter extends PaymentRequestWriter
 {
-
-    function write()
-    {
-        parent::write();
-
-        $this->updateAccountTo();
-
-        if ($this->saved->payment->status === 'completed')
-            $this->updateAccountsBalance();
-
-        return $this->saved;
-    }
-
-    /**
-     *Sets payment destination account.
-     */
-    private function updateAccountTo()
-    {
-        $this->data->item = PaymentItem::find($this->input->payment['paymentItemId']);
-        $this->data->duobAccount = LegalEntity::first()->accounts()->where('id', $this->input->payment['accountTo'])->first();
-        $this->data->userAccount = User::find($this->input->payment['payerId'])->accounts()->first();
-
+//
+//    function write()
+//    {
+//        parent::write();
+//
+//        $this->updateAccountTo();
+//
+//        if ($this->saved->payment->status === 'completed')
+//            $this->updateAccountsBalance();
+//
+//        return $this->saved;
+//    }
+//
+//    /**
+//     *Sets payment destination account.
+//     */
+//    private function updateAccountTo()
+//    {
+//        $this->data->item = PaymentItem::find($this->input->payment['paymentItemId']);
+//        $this->data->duobAccount = LegalEntity::first()->accounts()->where('id', $this->input->payment['accountTo'])->first();
+//        $this->data->userAccount = User::find($this->input->payment['payerId'])->accounts()->first();
+//
+////        if ($this->data->item->title === 'Пополнение баланса')
+////            $this->saved->payment->accountToId = $this->data->userAccount->id;
+////        else
+//        $this->saved->payment->accountToId = $this->data->duobAccount->id;
+//
+//        $this->saved->payment->save();
+//    }
+//
+//    /**
+//     *Updates related accounts balance. Duobs` balance is always updated
+//     */
+//    private function updateAccountsBalance()
+//    {
+//        $sum = $this->saved->payment->amount;
+//
+//        $this->data->exchange = MoneyExchange::find($this->saved->payment->exchangeId);
+//        if ($this->data->exchange)
+//            $sum = $sum * $this->data->exchange->coefficient;
+//
+//        $this->data->duobAccount->balance += $sum;
+//        $this->data->duobAccount->save();
+//
 //        if ($this->data->item->title === 'Пополнение баланса')
-//            $this->saved->payment->accountToId = $this->data->userAccount->id;
-//        else
-        $this->saved->payment->accountToId = $this->data->duobAccount->id;
-
-        $this->saved->payment->save();
-    }
-
-    /**
-     *Updates related accounts balance. Duobs` balance is always updated
-     */
-    private function updateAccountsBalance()
-    {
-        $sum = $this->saved->payment->amount;
-
-        $this->data->exchange = MoneyExchange::find($this->saved->payment->exchangeId);
-        if ($this->data->exchange)
-            $sum = $sum * $this->data->exchange->coefficient;
-
-        $this->data->duobAccount->balance += $sum;
-        $this->data->duobAccount->save();
-
-        if ($this->data->item->title === 'Пополнение баланса')
-            $this->updateUsersAccountBalance();
-    }
-
-    private function updateUsersAccountBalance()
-    {
-        $usd = Currency::where('isoName', 'USD')->first();
-        $sum = $this->saved->payment->amount;
-
-        if ($this->saved->payment->currency->id !== $usd->id) {
-            $exchange = $this->data->exchange;
-            if ($this->data->exchange->to !== $usd->id)
-                $exchange = MoneyExchange::where('from', $this->saved->payment->currency->id)
-                    ->where('to', $usd->id)->latest()->first();
-
-            $sum = $sum * $exchange->coefficient;
-        }
-
-        $this->data->userAccount->balance += $sum;
-        $this->data->userAccount->save();
-    }
+//            $this->updateUsersAccountBalance();
+//    }
+//
+//    private function updateUsersAccountBalance()
+//    {
+//        $usd = Currency::where('isoName', 'USD')->first();
+//        $sum = $this->saved->payment->amount;
+//
+//        if ($this->saved->payment->currency->id !== $usd->id) {
+//            $exchange = $this->data->exchange;
+//            if ($this->data->exchange->to !== $usd->id)
+//                $exchange = MoneyExchange::where('from', $this->saved->payment->currency->id)
+//                    ->where('to', $usd->id)->latest()->first();
+//
+//            $sum = $sum * $exchange->coefficient;
+//        }
+//
+//        $this->data->userAccount->balance += $sum;
+//        $this->data->userAccount->save();
+//    }
 }
--- app/Data/RequestWriters/Payments/OutgoingPaymentRequestWriter.php
+++ app/Data/RequestWriters/Payments/OutgoingPaymentRequestWriter.php
@@ -6,47 +6,47 @@ use App\Models\Till\Account;
 
 class OutgoingPaymentRequestWriter extends PaymentRequestWriter
 {
-    private $recipient;
-
-    public function __construct($recipient, $input)
-    {
-        $this->recipient = $recipient;
-        parent::__construct($input);
-    }
-
-    function write()
-    {
-        parent::write();
-
-        $this->updateAccountFromAndTo();
-
-        if ($this->saved->payment->status === 'completed')
-            $this->updateAccountsBalance();
-
-        return $this->saved;
-    }
-
-    /**
-     *Sets payment account.
-     */
-    private function updateAccountFromAndTo()
-    {
-//        if(!isset($this->data->duobAccount))
-//            $this->data->duobAccount = LegalEntity::first()->accounts()->with('currency')->first();
-        $this->data->accountFrom = Account::with('currency')->findOrFail($this->input->payment['accountFrom']);
-        $this->saved->payment->recipient_id = $this->recipient->id;
-        $this->saved->payment->accountFromId = $this->data->accountFrom->id;
-        $this->saved->payment->save();
-    }
-
-    /**
-     *Updates related accounts balance. Outgoing payment
-     * doesn`t support money exchange
-     */
-    private function updateAccountsBalance()
-    {
-        $sum = $this->saved->payment->amount;
-        $this->data->accountFrom->balance -= $sum;
-        $this->data->accountFrom->save();
-    }
+//    private $recipient;
+//
+//    public function __construct($recipient, $input)
+//    {
+//        $this->recipient = $recipient;
+//        parent::__construct($input);
+//    }
+//
+//    function write()
+//    {
+//        parent::write();
+//
+//        $this->updateAccountFromAndTo();
+//
+//        if ($this->saved->payment->status === 'completed')
+//            $this->updateAccountsBalance();
+//
+//        return $this->saved;
+//    }
+//
+//    /**
+//     *Sets payment account.
+//     */
+//    private function updateAccountFromAndTo()
+//    {
+////        if(!isset($this->data->duobAccount))
+////            $this->data->duobAccount = LegalEntity::first()->accounts()->with('currency')->first();
+//        $this->data->accountFrom = Account::with('currency')->findOrFail($this->input->payment['accountFrom']);
+//        $this->saved->payment->recipient_id = $this->recipient->id;
+//        $this->saved->payment->accountFromId = $this->data->accountFrom->id;
+//        $this->saved->payment->save();
+//    }
+//
+//    /**
+//     *Updates related accounts balance. Outgoing payment
+//     * doesn`t support money exchange
+//     */
+//    private function updateAccountsBalance()
+//    {
+//        $sum = $this->saved->payment->amount;
+//        $this->data->accountFrom->balance -= $sum;
+//        $this->data->accountFrom->save();
+//    }
 }
