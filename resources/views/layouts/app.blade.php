6cf18d5ed9478d1cd82ded66e52a86913fcbb613
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-02-18 11:20:08 +0500

    app blade nav update

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -88,10 +88,10 @@
                                     Касса <span class="caret"></span>
                                 </a>
                                 <div class="dropdown-menu" aria-labelledby="tillMenuDropdown">
-                                    <h6 class="dropdown-header">Провести платеж</h6>
+                                    <h6 class="dropdown-header">Платеж</h6>
                                     <a class="dropdown-item" href="{{route('payment.create')}}">Провести платеж</a>
-                                    <a class="dropdown-item" href="{{route('incoming-payments.create')}}">Приход</a>
-                                    <a class="dropdown-item" href="{{route('outgoing-payments.create')}}">Расход</a>
+{{--                                    <a class="dropdown-item" href="{{route('incoming-payments.create')}}">Приход</a>--}}
+{{--                                    <a class="dropdown-item" href="{{route('outgoing-payments.create')}}">Расход</a>--}}
                                     <div class="dropdown-divider"></div>
                                     <a class="dropdown-item" href="{{route('money-exchange.exchanger')}}">Обмен
                                         валют</a>
