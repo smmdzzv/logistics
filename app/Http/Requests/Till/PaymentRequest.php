e1d79438e99103ff9dc46404166d893e62dfd008
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-02-19 13:47:51 +0500

    payment request -> money exchange validation

--- app/Http/Requests/Till/PaymentRequest.php
+++ app/Http/Requests/Till/PaymentRequest.php
@@ -142,6 +142,9 @@ class PaymentRequest extends FormRequest
             case 'Перевод между счетами филиала':
                 $this->validateTransferBetweenBranchAccounts($validator);
                 break;
+            case 'Обмен валют':
+                $this->validateMoneyExchange($validator);
+                break;
         }
     }
 
@@ -162,7 +165,7 @@ class PaymentRequest extends FormRequest
         if (!($this->payee instanceof Branch))
             return $validator->errors()->add('payee', 'При переводе денег между филиалами получателем должен быть филиал. ');
 
-        if($this->payer->id === $this->payee->id)
+        if ($this->payer->id === $this->payee->id)
             return $validator->errors()->add('payee', 'При переводе денег между филиалами получатель и плательщик должны отличаться. ');
     }
 
@@ -173,5 +176,17 @@ class PaymentRequest extends FormRequest
 
         if ($this->request->get('billCurrency') === $this->request->get('paidCurrency'))
             return $validator->errors()->add('billCurrency', 'При переводе денег между счетами филиала валюта оплаты и валюта зачисления не должны совпадать. ');
+    }
+
+    private function validateMoneyExchange($validator)
+    {
+        if (!($this->payee instanceof Branch))
+            return $validator->errors()->add('payee', 'При обмене валют получателем должен быть филиал, а плательщиком клиент. ');
+        if (!($this->payer instanceof User))
+            return $validator->errors()->add('payer', 'При обмене валют плательщиком должен быть клиент, а получателем филиал. ');
+        if($this->request->get('billCurrency') === $this->request->get('paidCurrency'))
+            return $validator->errors()->add('billCurrency', 'При обмене валют требуемая валюта должна отличаться от валюты оплаты.');
+        if(!$this->request->get('exchangeRate'))
+            return $validator->errors()->add('exchangeRate', 'Курс обмена не указан. ');
     }
 }
