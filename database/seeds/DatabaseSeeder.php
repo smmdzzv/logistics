e537999302e419745bbd0ac3db63c2529f213294
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-02-21 11:44:07 +0500

    items table seeder update

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
          $this->call(CountriesTableSeeder::class);
          $this->call(BranchesTableSeeder::class);
          $this->call(TariffsTableSeeder::class);
-//         $this->call(ItemsTableSeeder::class);
+         $this->call(ItemsTableSeeder::class);
          $this->call(CurrenciesTableSeeder::class);
          $this->call(RolesTableSeeder::class);
          $this->call(PositionsTableSeeder::class);
