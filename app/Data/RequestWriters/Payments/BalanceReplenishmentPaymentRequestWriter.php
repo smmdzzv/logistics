764717f5b5e41a1d02db3cc27caa1bcd212c354b
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-02-24 14:34:10 +0500

    balance replenishment -> fixed

--- app/Data/RequestWriters/Payments/BalanceReplenishmentPaymentRequestWriter.php
+++ app/Data/RequestWriters/Payments/BalanceReplenishmentPaymentRequestWriter.php
@@ -6,15 +6,28 @@ namespace App\Data\RequestWriters\Payments;
 
 class BalanceReplenishmentPaymentRequestWriter extends PaymentRequestWriter
 {
-    protected function getAccounts()
-    {
-        //Users have only dollar account. All operations should be in dollars
-        $this->payerAccount = $this->payer->accounts()->dollarAccount();
-        if (!$this->payerAccount)
-            abort('400', 'Пользователь не имеет долларового счета');
+//    protected function getAccounts()
+//    {
+//        parent::getAccounts();
+//
+//        $this->payeeAccountInSecondCurrency = null;
+//        $this->payeeAccountInBillCurrency =
+//        //Users have only dollar account. All operations should be in dollars
+//        $this->payerAccount = $this->payer->accounts()->dollarAccount();
+//        if (!$this->payerAccount)
+//            abort('400', 'Пользователь не имеет долларового счета');
+//
+//        $this->payeeAccount = $this->getSubjectAccount($this->payee);
+//    }
 
-        $this->payeeAccount = $this->getSubjectAccount($this->payee);
+    //Users have only dollar account. All operations should be in dollars
+    protected function getPayerAccounts()
+    {
+        $this->payerAccountInBillCurrency = $this->payer->accounts()->dollarAccount();
+        if (!$this->payerAccountInBillCurrency)
+            abort('422', 'Пользователь не имеет долларового счета');
     }
+
 
     protected function checkPayerBalance()
     {
@@ -23,8 +36,8 @@ class BalanceReplenishmentPaymentRequestWriter extends PaymentRequestWriter
 
     protected function updatePayerBalance()
     {
-        $this->payerAccount->balance += $this->payment->billAmount;
-        $this->payerAccount->save();
+        $this->payerAccountInBillCurrency->balance += $this->payment->billAmount;
+        $this->payerAccountInBillCurrency->save();
     }
 
 }
