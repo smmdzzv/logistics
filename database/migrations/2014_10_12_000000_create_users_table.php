eaab363d1f8c04f2b863bcf79a7f99cd0280a251
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-03-12 00:57:38 +0500

    timezone

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('email')->unique()->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('timezone')->nullable();
             $table->string('api_token', 80)
                 ->unique()
                 ->nullable()
