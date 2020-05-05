f1dc301a4093e323904b89cb3877d522dc5471dc
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-05-05 14:09:04 +0500

    hide price histories if tariff was deleted

--- app/Http/Controllers/TariffPriceHistoriesController.php
+++ app/Http/Controllers/TariffPriceHistoriesController.php
@@ -22,6 +22,7 @@ class TariffPriceHistoriesController extends Controller
     {
         $paginate = request()->paginate ?? 10;
         return TariffPriceHistory::with('tariff')
+            ->whereHas('tariff')
             ->orderBy('created_at', 'desc')
             ->paginate($paginate);
     }
