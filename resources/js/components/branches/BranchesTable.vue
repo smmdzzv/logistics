125ff214c7ffe014b19abed6ee3f5b8ce8e59143
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-07-12 14:47:28 +0500

    table fix

--- resources/js/components/branches/BranchesTable.vue
+++ resources/js/components/branches/BranchesTable.vue
@@ -11,14 +11,14 @@
                 <b-spinner class="align-middle"></b-spinner>
             </div>
         </template>
-        <template slot="director.name" slot-scope="data">
+        <template v-slot:cell(director.name)="data">
             <span v-if="data.item.director">{{data.item.director.name}}</span>
             <span v-else>Не назначен</span>
         </template>
-        <template slot="edit" slot-scope="data">
+        <template v-slot:cell(edit)="data">
             <button @click="onEditRequest(data.item)" class="btn btn-outline-secondary">Изменить</button>
         </template>
-        <template slot="remove" slot-scope="data">
+        <template v-slot:cell(remove)="data">
             <img @click="deleteBranch(data.item)"
                  alt="удалить тариф"
                  class="icon-btn-sm"
@@ -30,37 +30,33 @@
 <script>
     export default {
         name: "BranchesTable",
-        props:{
-            branches:{
-                type:Array,
-                required:true
+        props: {
+            branches: {
+                type: Array,
+                required: true
             },
-            onEditRequest:{
-                type:Function,
-                required:true
+            onEditRequest: {
+                type: Function,
+                required: true
             }
         },
-        created(){
-            // this.getBranches()
-        },
-        methods:{
-            async deleteBranch(branch){
-                console.log(branch);
-                let confirm = await this.$bvModal.msgBoxConfirm(`Вы уверены что хотите удалить филиал ${branch.name}?`, {
-                    centered: true,
-                    okTitle: 'Да',
-                    cancelTitle: 'Отменить',
-                    footerClass: 'border-0',
-                    title: 'Подтверждение удаления'
-                });
+        methods: {
+            async deleteBranch(branch) {
+                let confirm = await this.$bvModal.msgBoxConfirm(
+                    `Вы уверены что хотите удалить филиал ${branch.name}?`,
+                    {
+                        centered: true,
+                        okTitle: 'Да',
+                        cancelTitle: 'Отменить',
+                        footerClass: 'border-0',
+                        title: 'Подтверждение удаления'
+                    });
 
-                if(confirm){
-                    try{
+                if (confirm) {
+                    try {
                         const response = axios.delete('/branches/' + branch.id);
-                        console.log(response);
                         this.$emit('branchDeleted', branch)
-                    }
-                    catch (e) {
+                    } catch (e) {
                         this.$root.showErrorMsg('Ошибка удаления',
                             'Не удалось удалить филиал. Обновите страницу и попробуйте удалить еще раз')
                     }
@@ -68,26 +64,30 @@
 
             }
         },
-        data(){
-            return{
-                items:[],
-                isBusy:false,
-                fields:{
-                    name:{
-                        label:'Имя',
-                        sortable:true
+        data() {
+            return {
+                items: [],
+                isBusy: false,
+                fields: [
+                    {
+                        key: 'name',
+                        label: 'Имя',
+                        sortable: true
                     },
-                    'director.name':{
-                        label:'Директор',
-                        sortable:true
+                    {
+                        key: 'director.name',
+                        label: 'Директор',
+                        sortable: true
                     },
-                    'edit':{
-                        label:'Изменить'
+                    {
+                        key: 'edit',
+                        label: 'Изменить'
                     },
-                    'remove':{
+                    {
+                        key: 'remove',
                         label: ''
                     }
-                }
+                ]
             }
         }
     }
