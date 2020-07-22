76beb41bfaccc7d9502c102589abb2a6d1f64ba8
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2020-07-22 20:36:46 +0500

    removed hidden

--- resources/views/orders/show.blade.php
+++ resources/views/orders/show.blade.php
@@ -13,37 +13,37 @@
         </div>
     </div>
 
-    @if(count($order->orderRemovedItems) > 0)
-        <div class="row justify-content-center">
-            <div class="col-12 px-md-5">
-                <div class="card shadow">
-                    <div class="card-header">Удаленные позиции</div>
-                    <table class="table table-striped">
-                        <thead>
-                        <tr>
-                            <th scope="col">Тип</th>
-                            <th scope="col">Кол-во</th>
-                            <th scope="col">Вес</th>
-                            <th scope="col">Объем, м <sup>3</sup></th>
-                            <th scope="col">Удалено</th>
-                            <th scope="col">Удалил</th>
-                        </tr>
-                        </thead>
-                        <tbody>
-                        @foreach($order->orderRemovedItems as $removed)
-                            <tr>
-                                <td>{{$removed->storedItemInfo->item->name}}</td>
-                                <td>{{$removed->storedItemInfo->count}}</td>
-                                <td>{{$removed->storedItemInfo->weight * $removed->storedItemInfo->count}}</td>
-                                <td>{{round($removed->storedItemInfo->length * $removed->storedItemInfo->height * $removed->storedItemInfo->width * $removed->storedItemInfo->count,2)}}</td>
-                                <td>{{$removed->storedItemInfo->deleted_at}}</td>
-                                <td>{{$removed->storedItemInfo->deletedBy->name}}</td>
-                            </tr>
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-    @endif
+{{--    @if(count($order->orderRemovedItems) > 0)--}}
+{{--        <div class="row justify-content-center">--}}
+{{--            <div class="col-12 px-md-5">--}}
+{{--                <div class="card shadow">--}}
+{{--                    <div class="card-header">Удаленные позиции</div>--}}
+{{--                    <table class="table table-striped">--}}
+{{--                        <thead>--}}
+{{--                        <tr>--}}
+{{--                            <th scope="col">Тип</th>--}}
+{{--                            <th scope="col">Кол-во</th>--}}
+{{--                            <th scope="col">Вес</th>--}}
+{{--                            <th scope="col">Объем, м <sup>3</sup></th>--}}
+{{--                            <th scope="col">Удалено</th>--}}
+{{--                            <th scope="col">Удалил</th>--}}
+{{--                        </tr>--}}
+{{--                        </thead>--}}
+{{--                        <tbody>--}}
+{{--                        @foreach($order->orderRemovedItems as $removed)--}}
+{{--                            <tr>--}}
+{{--                                <td>{{$removed->storedItemInfo->item->name}}</td>--}}
+{{--                                <td>{{$removed->storedItemInfo->count}}</td>--}}
+{{--                                <td>{{$removed->storedItemInfo->weight * $removed->storedItemInfo->count}}</td>--}}
+{{--                                <td>{{round($removed->storedItemInfo->length * $removed->storedItemInfo->height * $removed->storedItemInfo->width * $removed->storedItemInfo->count,2)}}</td>--}}
+{{--                                <td>{{$removed->storedItemInfo->deleted_at}}</td>--}}
+{{--                                <td>{{$removed->storedItemInfo->deletedBy->name}}</td>--}}
+{{--                            </tr>--}}
+{{--                        @endforeach--}}
+{{--                        </tbody>--}}
+{{--                    </table>--}}
+{{--                </div>--}}
+{{--            </div>--}}
+{{--        </div>--}}
+{{--    @endif--}}
 @endsection
