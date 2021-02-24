8dadc28afefceea20feb48cccc7ac4aa70c4cb3d
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2021-02-24 20:46:42 +0500

    fix table

--- resources/js/components/stored/StoredTable.vue
+++ resources/js/components/stored/StoredTable.vue
@@ -22,7 +22,7 @@
                             <select class="form-control custom-select" id="branch" v-model="selectedBranch">
                                 <option :value="null">--Все склады--</option>
                                 <option :key="branch.id" :value="branch" v-for="branch in branches">
-                                    {{branch.name}}
+                                    {{ branch.name }}
                                 </option>
                             </select>
                         </div>
@@ -62,16 +62,16 @@
                 </div>
             </template>
 
-            <template slot="owner" slot-scope="{item}">
-                <span>{{item.info.owner.code}} {{item.info.owner.name}}</span>
+            <template v-slot:cell(owner)="{item}">
+                <span>{{ item.info.owner.code }} {{ item.info.owner.name }}</span>
             </template>
 
-            <template slot="buttons" slot-scope="data">
-                <a :href="'/stored/' + data.item.id"><img class="icon-btn-sm" src="/svg/file.svg"></a>
+            <template v-slot:cell(buttons)="{item}">
+                <a :href="'/stored/' + item.id"><img class="icon-btn-sm" src="/svg/file.svg"></a>
             </template>
 
-            <template slot="selected" slot-scope="data">
-                <span class="text-success" v-if="isSelected(data.item)">&check;</span>
+            <template v-slot:cell(selected)="{item}">
+                <span class="text-success" v-if="isSelected(item)">&check;</span>
                 <span v-else></span>
             </template>
         </b-table>
@@ -84,247 +84,257 @@
 </template>
 
 <script>
-    import ExcelDataPreparatory from '../common/ExcelDataPreparatory.vue'
+import ExcelDataPreparatory from '../common/ExcelDataPreparatory.vue'
 
-    export default {
-        name: "StoredTable",
-        mixins: [ExcelDataPreparatory],
-        mounted() {
+export default {
+    name: "StoredTable",
+    mixins: [ExcelDataPreparatory],
+    mounted() {
+        this.setItems();
+        this.getItems();
+    },
+    props: {
+        branches: {
+            type: Array,
+            required: false,
+            default: null
+        },
+        listGenerator: {
+            type: Boolean
+        },
+        selectable: {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+        flowable: {
+            type: Boolean,
+            required: false,
+            default: false
+        },
+        loadData: {
+            type: Boolean,
+            required: false,
+            default: true
+        },
+        selectedItems: {
+            type: Array,
+            required: false,
+            default: () => []
+        },
+        url: {
+            type: String,
+            required: false,
+            default: '/stored/all'
+        },
+        providedItems: {
+            type: Array,
+            required: false,
+        },
+        striped: {
+            type: Boolean,
+            required: false,
+            default: true
+        },
+        hover: {
+            type: Boolean,
+            required: false,
+        },
+        prepareUrl: {
+            type: Function,
+            default: (page, vm) => {
+                let action = vm.url;
+                if (vm.selectedBranch)
+                    action = `/${vm.selectedBranch.id}/stored`;
+                return action += '?paginate=7&page=' + page;
+            }
+        },
+        tripId: {
+            type: String
+        },
+        highlightRows: {
+            type: Boolean,
+            default: false
+        },
+        excelExport: {
+            type: Boolean,
+            default: false
+        }
+    },
+    methods: {
+        // Checks if item in all stored items array
+        isInItems(item) {
+            return this.items.find(function (stored) {
+                return stored.id === item.id;
+            });
+        },
+        // Checks if item in provided list stored items,
+        // which are items belonging to certain trip
+        isInProvidedItems(item) {
+            return this.providedItems.find(function (stored) {
+                return stored.id === item.id;
+            });
+        },
+        getItems(page = 1) {
+            if (!this.loadData)
+                return;
+
+            this.isBusy = true;
+
+            let action = this.prepareUrl(page, this);
+
+            axios.get(action)
+                .then(response => {
+                    this.pagination = response.data;
+                    let items = response.data.data.filter(item => {
+                        return !this.isInItems(item)
+                    });
+
+                    if (this.flowable)
+                        items.forEach(item => {
+                            this.items.push(item);
+                        });
+                    else {
+                        this.items = [...this.items, ...items];
+                    }
+                    this.$nextTick(() => {
+                        this.isBusy = false;
+                    })
+                });
+        },
+        itemSelected(item) {
+            if (!this.selectable)
+                return;
+            if (this.isSelected(item)) {
+                return this.$emit('onItemUnselected', item);
+            } else {
+                return this.$emit('onItemSelected', item);
+            }
+        },
+        isSelected(item) {
+            return this.selectedItems.find(function (selected) {
+                return selected.id === item.id;
+            });
+        },
+        rowClass(item, type) {
+            if (!this.highlightRows || !item) return;
+            if (this.isSelected(item))
+                return 'table-success';
+            else if (this.isInProvidedItems(item))
+                return 'table-danger';
+        },
+        setItems() {
+            if (this.providedItems)
+                for (let item of this.providedItems) {
+                    this.items.push(item);
+                }
+
+            if (this.selectedItems)
+                for (let item of this.selectedItems) {
+                    if (!this.isInItems(item))
+                        this.items.push(item);
+                }
+        },
+        async generateList() {
+            tShowSpinner();
+            try {
+                let action = `/trip/${this.tripId}/stored-items/generate`;
+                if (this.selectedBranch)
+                    action += `?branch=${this.selectedBranch.id}`;
+                const response = await axios.get(action);
+                // this.items = response.data;
+                for (let item of response.data) {
+                    if (!this.isInItems(item))
+                        this.items.push(item);
+
+                }
+
+                this.$emit('onItemsSelected', response.data);
+
+            } catch (e) {
+                this.$root.showErrorMsg(
+                    "Ошибка генерации",
+                    'Не удалось сгенерировать список. Попробуйте сгенерировать список позднее'
+                )
+            }
+
+            tHideSpinner();
+        }
+    },
+    computed: {
+        currentPage() {
+            return this.pagination.current_page;
+        },
+        lastPage() {
+            return this.pagination.last_page
+        }
+    },
+    watch: {
+        selectedBranch: function () {
+            this.items.splice(0, this.items.length);
             this.setItems();
             this.getItems();
         },
-        props: {
-            branches: {
-                type: Array,
-                required: false,
-                default: null
+    },
+    data() {
+        return {
+            selectedBranch: null,
+            pagination: {
+                last_page: null,
+                current_page: null
             },
-            listGenerator:{
-                type:Boolean
-            },
-            selectable: {
-                type: Boolean,
-                required: false,
-                default: false
-            },
-            flowable: {
-                type: Boolean,
-                required: false,
-                default: false
-            },
-            loadData: {
-                type: Boolean,
-                required: false,
-                default: true
-            },
-            selectedItems: {
-                type: Array,
-                required: false,
-                default: () => []
-            },
-            url: {
-                type: String,
-                required: false,
-                default: '/stored/all'
-            },
-            providedItems: {
-                type: Array,
-                required: false,
-            },
-            striped: {
-                type: Boolean,
-                required: false,
-                default: true
-            },
-            hover: {
-                type: Boolean,
-                required: false,
-            },
-            prepareUrl: {
-                type: Function,
-                default: (page, vm) => {
-                    let action = vm.url;
-                    if (vm.selectedBranch)
-                        action = `/${vm.selectedBranch.id}/stored`;
-                    return action += '?paginate=7&page=' + page;
-                }
-            },
-            tripId: {
-                type: String
-            },
-            highlightRows: {
-                type: Boolean,
-                default: false
-            },
-            excelExport: {
-                type: Boolean,
-                default: false
-            }
-        },
-        methods: {
-            // Checks if item in all stored items array
-            isInItems(item) {
-                return this.items.find(function (stored) {
-                    return stored.id === item.id;
-                });
-            },
-            // Checks if item in provided list stored items,
-            // which are items belonging to certain trip
-            isInProvidedItems(item) {
-                return this.providedItems.find(function (stored) {
-                    return stored.id === item.id;
-                });
-            },
-            getItems(page = 1) {
-                if (!this.loadData)
-                    return;
-
-                this.isBusy = true;
-
-                let action = this.prepareUrl(page, this);
-
-                axios.get(action)
-                    .then(response => {
-                        this.pagination = response.data;
-                        let items = response.data.data.filter(item => {
-                            return !this.isInItems(item)
-                        });
-
-                        if (this.flowable)
-                            items.forEach(item => {
-                                this.items.push(item);
-                            });
-                        else {
-                            this.items = [...this.items, ...items];
-                        }
-                        this.$nextTick(() => {
-                            this.isBusy = false;
-                        })
-                    });
-            },
-            itemSelected(item) {
-                if (!this.selectable)
-                    return;
-                if (this.isSelected(item)) {
-                    return this.$emit('onItemUnselected', item);
-                } else {
-                    return this.$emit('onItemSelected', item);
-                }
-            },
-            isSelected(item) {
-                return this.selectedItems.find(function (selected) {
-                    return selected.id === item.id;
-                });
-            },
-            rowClass(item, type) {
-                if (!this.highlightRows || !item) return;
-                if (this.isSelected(item))
-                    return 'table-success';
-                else if (this.isInProvidedItems(item))
-                    return 'table-danger';
-            },
-            setItems() {
-                if (this.providedItems)
-                    for (let item of this.providedItems) {
-                        this.items.push(item);
-                    }
-
-                if (this.selectedItems)
-                    for (let item of this.selectedItems) {
-                        if (!this.isInItems(item))
-                            this.items.push(item);
-                    }
-            },
-            async generateList() {
-                tShowSpinner();
-                try {
-                    let action = `/trip/${this.tripId}/stored-items/generate`;
-                    if(this.selectedBranch)
-                        action += `?branch=${this.selectedBranch.id}`;
-                    const response = await axios.get(action);
-                    // this.items = response.data;
-                    for (let item of response.data) {
-                        if (!this.isInItems(item))
-                            this.items.push(item);
-
-                    }
-
-                    this.$emit('onItemsSelected', response.data);
-
-                } catch (e) {
-                    this.$root.showErrorMsg(
-                        "Ошибка генерации",
-                        'Не удалось сгенерировать список. Попробуйте сгенерировать список позднее'
-                    )
-                }
-
-                tHideSpinner();
-            }
-        },
-        computed: {
-            currentPage() {
-                return this.pagination.current_page;
-            },
-            lastPage() {
-                return this.pagination.last_page
-            }
-        },
-        watch: {
-            selectedBranch: function () {
-                this.items.splice(0, this.items.length);
-                this.setItems();
-                this.getItems();
-            },
-        },
-        data() {
-            return {
-                selectedBranch: null,
-                pagination: {
-                    last_page: null,
-                    current_page: null
+            items: [],
+            isBusy: false,
+            fields: [
+                {
+                    key: 'owner',
+                    label: 'Владелец',
+                    sortable: true
                 },
-                items: [],
-                isBusy: false,
-                fields: {
-                    'owner': {
-                        label: 'Владелец',
-                        sortable: true
-                    },
-                    'info.item.name': {
-                        label: 'Наименование',
-                        sortable: true
-                    },
-                    code: {
-                        label: 'Код',
-                        sortable: true
-                    },
-                    'info.width': {
-                        label: 'Ширина',
-                        sortable: true
-                    },
-                    'info.height': {
-                        label: 'Высота',
-                        sortable: true
-                    },
-                    'info.length': {
-                        label: 'Длина',
-                        sortable: true
-                    },
-                    'info.weight': {
-                        label: 'Вес',
-                        sortable: true
-                    },
-                    'storageHistory.storage.name': {
-                        label: 'Склад'
-                    },
-                    'buttons':{
-                        label:''
-                    },
-                    'selected': {
-                        label: ''
-                    }
+                {
+                    key: 'info.item.name',
+                    label: 'Наименование',
+                    sortable: true
+                },
+                {
+                    key: 'code',
+                    label: 'Код',
+                    sortable: true
+                },
+                {
+                    key: 'info.width',
+                    label: 'Ширина',
+                    sortable: true
+                },
+                {
+                    key: 'info.height',
+                    label: 'Высота',
+                    sortable: true
+                },
+                {
+                    key: 'info.length',
+                    label: 'Длина',
+                    sortable: true
+                },
+                {
+                    key: 'info.weight',
+                    label: 'Вес',
+                    sortable: true
+                },
+                {
+                    key: 'storageHistory.storage.name',
+                    label: 'Склад'
+                },
+                {
+                    key: 'buttons',
+                    label: ''
+                },
+                {
+                    key: 'selected',
+                    label: ''
                 }
-            }
+            ]
         }
     }
+}
 </script>
