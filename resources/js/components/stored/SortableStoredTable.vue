575bcfd83a6682884145e96ffcc1224aba6a6954
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2021-03-27 18:07:08 +0500

    show status in case of trip only

--- resources/js/components/stored/SortableStoredTable.vue
+++ resources/js/components/stored/SortableStoredTable.vue
@@ -18,7 +18,7 @@
                     <select class="form-control custom-select" id="branch" v-model="selectedClient">
                         <option :value="null">--Все клиенты--</option>
                         <option :key="client.id" :value="client" v-for="client in clients">
-                            {{client.code}}
+                            {{ client.code }}
                         </option>
                     </select>
                 </div>
@@ -50,27 +50,27 @@
             </template>
 
             <template v-slot:cell(owner)="{item}">
-                <span>{{item.info.owner.code}} {{item.info.owner.name}}</span>
+                <span>{{ item.info.owner.code }} {{ item.info.owner.name }}</span>
             </template>
 
             <template v-slot:cell(info.weight)="{item}">
-                <span>{{item.info.weight.toFixed(3)}}</span>
+                <span>{{ item.info.weight.toFixed(3) }}</span>
             </template>
 
             <template v-slot:cell(info.height)="{item}">
-                <span>{{item.info.height.toFixed(3)}}</span>
+                <span>{{ item.info.height.toFixed(3) }}</span>
             </template>
 
             <template v-slot:cell(info.length)="{item}">
-                <span>{{item.info.length.toFixed(3)}}</span>
+                <span>{{ item.info.length.toFixed(3) }}</span>
             </template>
 
             <template v-slot:cell(info.width)="{item}">
-                <span>{{item.info.width.toFixed(3)}}</span>
+                <span>{{ item.info.width.toFixed(3) }}</span>
             </template>
 
             <template v-slot:cell(info.cubage)="{item}">
-                <span>{{calculateCubage(item.info).toFixed(3)}}</span>
+                <span>{{ calculateCubage(item.info).toFixed(3) }}</span>
             </template>
 
             <template v-slot:cell(tripHistory.status)="{item}">
@@ -105,211 +105,218 @@
 </template>
 
 <script>
-    import ExcelDataPreparatory from '../common/ExcelDataPreparatory.vue'
+import ExcelDataPreparatory from '../common/ExcelDataPreparatory.vue'
 
-    export default {
-        name: "SortableStoredTable",
-        mixins: [ExcelDataPreparatory],
-        mounted() {
-            this.items = this.storedItems;
-            this.extractClients();
-            this.calculateDutyPrices();
+export default {
+    name: "SortableStoredTable",
+    mixins: [ExcelDataPreparatory],
+    mounted() {
+        this.items = this.storedItems;
+        this.extractClients();
+        this.calculateDutyPrices();
+
+        if (this.trip)
+            this.fields.push({
+                key: 'tripHistory.status',
+                label: 'Статус товара'
+            })
+    },
+    props: {
+        storedItems: {
+            type: Array,
+            required: true
         },
-        props: {
-            storedItems: {
-                type: Array,
-                required: true
-            },
-            trip: {
-                type: Object,
-                required: true
-            }
+        trip: {
+            type: Object,
+            required: true
+        }
+    },
+    methods: {
+        extractClients() {
+            this.storedItems.forEach((item) => {
+                if (!this.clients.find(x => x.id === item.info.owner.id)) {
+                    this.clients.push(item.info.owner);
+                }
+            }, this);
         },
-        methods: {
-            extractClients() {
-                this.storedItems.forEach((item) => {
-                    if (!this.clients.find(x => x.id === item.info.owner.id)) {
-                        this.clients.push(item.info.owner);
-                    }
-                }, this);
-            },
-            // groupByTariffAndCode() {
-            //     let groupedByTariff = groupBy(this.items, i => i.info.tariff.id);
-            //     let groupedByCode = [];
-            //
-            //     groupedByTariff.forEach((arr) => {
-            //         let grouped = groupBy(arr, i => i.info.customsCode.id);
-            //         grouped.forEach((arr) => {
-            //             groupedByCode.push(...arr);
-            //         })
-            //     });
-            //
-            //     this.items = groupedByCode;
-            //     this.fillGroupedData();
-            //     $('#grouped-data').click();
-            // },
-            fillGroupedData() {
-                let groupedByCode = groupBy(this.items, i => i.info.customsCode.id);
-                groupedByCode.forEach((arr) => {
-                    let totalDutyPrice = 0;
-                    let totalWeight = 0;
-                    let count = 0;
-                    let totalPrice = 0;
+        // groupByTariffAndCode() {
+        //     let groupedByTariff = groupBy(this.items, i => i.info.tariff.id);
+        //     let groupedByCode = [];
+        //
+        //     groupedByTariff.forEach((arr) => {
+        //         let grouped = groupBy(arr, i => i.info.customsCode.id);
+        //         grouped.forEach((arr) => {
+        //             groupedByCode.push(...arr);
+        //         })
+        //     });
+        //
+        //     this.items = groupedByCode;
+        //     this.fillGroupedData();
+        //     $('#grouped-data').click();
+        // },
+        fillGroupedData() {
+            let groupedByCode = groupBy(this.items, i => i.info.customsCode.id);
+            groupedByCode.forEach((arr) => {
+                let totalDutyPrice = 0;
+                let totalWeight = 0;
+                let count = 0;
+                let totalPrice = 0;
 
-                    arr.forEach((item) => {
-                        totalDutyPrice += item.dutyPrice;
-                        totalWeight += item.info.weight;
-                        totalPrice += item.info.billingInfo.pricePerItem;
-                        count++;
-                    });
-
-                    let customsCode = arr[0].info.customsCode;
-
-                    this.groupedData.push({
-                        name: customsCode.name,
-                        code: customsCode.code,
-                        count: count,
-                        totalWeight: totalWeight,
-                        totalPrice: totalPrice,
-                        averagePricePerItem: Math.round(totalPrice / count * 100) / 100,
-                        totalDutyPrice: totalDutyPrice
-                    });
+                arr.forEach((item) => {
+                    totalDutyPrice += item.dutyPrice;
+                    totalWeight += item.info.weight;
+                    totalPrice += item.info.billingInfo.pricePerItem;
+                    count++;
                 });
-            },
-            calculateDutyPrices() {
-                this.items.forEach((item) => {
-                    let dutyPrice = 0;
-                    let customsTariff = item.info.customsCode;
-                    if (customsTariff.isCalculatedByPiece) {
-                        dutyPrice = customsTariff.price * customsTariff.totalRate / 100
-                    } else {
-                        let tonnage = item.info.weight / 1000;
-                        dutyPrice = tonnage * customsTariff.price * customsTariff.totalRate / 100
-                    }
-                    item.dutyPrice = Math.round(dutyPrice * 100) / 100;
-                })
-            },
-            calculateCubage(info) {
-                return info.cubage = Math.round(info.height * info.width * info.length * 100) / 100;
-            }
+
+                let customsCode = arr[0].info.customsCode;
+
+                this.groupedData.push({
+                    name: customsCode.name,
+                    code: customsCode.code,
+                    count: count,
+                    totalWeight: totalWeight,
+                    totalPrice: totalPrice,
+                    averagePricePerItem: Math.round(totalPrice / count * 100) / 100,
+                    totalDutyPrice: totalDutyPrice
+                });
+            });
         },
-        watch: {
-            selectedClient() {
-                if (this.selectedClient)
-                    this.items = this.storedItems.filter((item) => {
-                        return item.info.owner.id === this.selectedClient.id
-                    }, this);
-                else
-                    this.items = this.storedItems;
-            }
+        calculateDutyPrices() {
+            this.items.forEach((item) => {
+                let dutyPrice = 0;
+                let customsTariff = item.info.customsCode;
+                if (customsTariff.isCalculatedByPiece) {
+                    dutyPrice = customsTariff.price * customsTariff.totalRate / 100
+                } else {
+                    let tonnage = item.info.weight / 1000;
+                    dutyPrice = tonnage * customsTariff.price * customsTariff.totalRate / 100
+                }
+                item.dutyPrice = Math.round(dutyPrice * 100) / 100;
+            })
         },
-        data() {
-            return {
-                items: [],
-                excelColumns: [],
-                excelData: [],
-                isBusy: false,
-                selectedClient: null,
-                clients: [],
-                groupedData: [],
-                fields: [
-                    {
-                        key: 'owner',
-                        label: 'Владелец',
-                        sortable: true
-                    },
-                    {
-                        key: 'info.item.name',
-                        label: 'Наименование',
-                        sortable: true
-                    },
-                    {
-                        key: 'code',
-                        label: 'Код',
-                        sortable: true
-                    },
-                    {
-                        key: 'info.tariff.name',
-                        label: 'Тариф',
-                        sortable: true
-                    },
-                    {
-                        key: 'info.customsCode.code',
-                        label: 'Там. код',
-                        sortable: true
-                    },
-                    {
-                        key: 'info.width',
-                        label: 'Ширина',
-                        sortable: true
-                    },
-                    {
-                        key: 'info.height',
-                        label: 'Высота',
-                        sortable: true
-                    },
-                    {
-                        key: 'info.length',
-                        label: 'Длина',
-                        sortable: true
-                    },
-                    {
-                        key: 'info.cubage',
-                        label: 'Кубатура',
-                        sortable: true
-                    },
-                    {
-                        key: 'info.weight',
-                        label: 'Вес',
-                        sortable: true
-                    },
-                    {
-                        key: 'storageHistory.storage.name',
-                        label: 'Склад'
-                    },
-                    // {
-                    //     key: 'dutyPrice',
-                    //     label: 'Таможенная пошлина'
-                    // }
-                    {
-                        key: 'tripHistory.status',
-                        label: 'Статус товара'
-                    }
-                ],
-                groupedDataColumns: [
-                    {
-                        label: 'Наименование',
-                        field: 'name'
-                    },
-                    {
-                        label: 'Код',
-                        field: 'code'
-                    },
-                    {
-                        label: 'Количество',
-                        field: 'count'
-                    },
-                    {
-                        label: 'Вес',
-                        field: 'totalWeight'
-                    },
-                    {
-                        label: 'Средняя цена за штуку',
-                        field: 'averagePricePerItem'
-                    },
-                    {
-                        label: 'Общая стоимость',
-                        field: 'totalPrice'
-                    },
-                    {
-                        label: 'Таможенная пошлина',
-                        field: 'totalDutyPrice'
-                    }
-                ]
-            }
-        },
-        components: {
-            'StoredTable': require('./StoredTable.vue').default
-        },
-    }
+        calculateCubage(info) {
+            return info.cubage = Math.round(info.height * info.width * info.length * 100) / 100;
+        }
+    },
+    watch: {
+        selectedClient() {
+            if (this.selectedClient)
+                this.items = this.storedItems.filter((item) => {
+                    return item.info.owner.id === this.selectedClient.id
+                }, this);
+            else
+                this.items = this.storedItems;
+        }
+    },
+    data() {
+        return {
+            items: [],
+            excelColumns: [],
+            excelData: [],
+            isBusy: false,
+            selectedClient: null,
+            clients: [],
+            groupedData: [],
+            fields: [
+                {
+                    key: 'owner',
+                    label: 'Владелец',
+                    sortable: true
+                },
+                {
+                    key: 'info.item.name',
+                    label: 'Наименование',
+                    sortable: true
+                },
+                {
+                    key: 'code',
+                    label: 'Код',
+                    sortable: true
+                },
+                {
+                    key: 'info.tariff.name',
+                    label: 'Тариф',
+                    sortable: true
+                },
+                {
+                    key: 'info.customsCode.code',
+                    label: 'Там. код',
+                    sortable: true
+                },
+                {
+                    key: 'info.width',
+                    label: 'Ширина',
+                    sortable: true
+                },
+                {
+                    key: 'info.height',
+                    label: 'Высота',
+                    sortable: true
+                },
+                {
+                    key: 'info.length',
+                    label: 'Длина',
+                    sortable: true
+                },
+                {
+                    key: 'info.cubage',
+                    label: 'Кубатура',
+                    sortable: true
+                },
+                {
+                    key: 'info.weight',
+                    label: 'Вес',
+                    sortable: true
+                },
+                {
+                    key: 'storageHistory.storage.name',
+                    label: 'Склад'
+                },
+                // {
+                //     key: 'dutyPrice',
+                //     label: 'Таможенная пошлина'
+                // }
+                //In case of trip added in mount
+                // {
+                //     key: 'tripHistory.status',
+                //     label: 'Статус товара'
+                // }
+            ],
+            groupedDataColumns: [
+                {
+                    label: 'Наименование',
+                    field: 'name'
+                },
+                {
+                    label: 'Код',
+                    field: 'code'
+                },
+                {
+                    label: 'Количество',
+                    field: 'count'
+                },
+                {
+                    label: 'Вес',
+                    field: 'totalWeight'
+                },
+                {
+                    label: 'Средняя цена за штуку',
+                    field: 'averagePricePerItem'
+                },
+                {
+                    label: 'Общая стоимость',
+                    field: 'totalPrice'
+                },
+                {
+                    label: 'Таможенная пошлина',
+                    field: 'totalDutyPrice'
+                }
+            ]
+        }
+    },
+    components: {
+        'StoredTable': require('./StoredTable.vue').default
+    },
+}
 </script>
