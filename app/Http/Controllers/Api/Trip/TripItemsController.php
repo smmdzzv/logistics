0e4dbf8dd4e81720b87f73926055a1cf028f95db
Sultonazar Mamadazizov <sultonazar.mamadazizov@mail.ru>
2021-04-10 23:23:25 +0500

    owner

--- app/Http/Controllers/Api/Trip/TripItemsController.php
+++ app/Http/Controllers/Api/Trip/TripItemsController.php
@@ -24,7 +24,10 @@ class TripItemsController extends Controller
         return $trip->storedItems()
             ->select(['stored_items.id', 'stored_item_info_id', 'code', 'stored_items.status'])
             ->with(['info' => function($query){
-                $query->select(['id', 'shop', 'count', 'weight', 'height', 'length', 'width']);
+                $query->select(['id', 'shop', 'count', 'weight', 'height', 'length', 'width','owner_id'])
+                    ->with(['owner' => function($query){
+                        $query->select(['id', 'code']);
+                    }]);
             }])
             ->whereIn('stored_items.status', [
                 StoredItem::STATUS_STORED,
@@ -32,7 +35,9 @@ class TripItemsController extends Controller
             ])->get()->map(function (StoredItem $storedItem){
                 $storedItem->trip_status = $storedItem->tripHistory->status;
                 $storedItem->trip_id = $storedItem->tripHistory->trip_id;
+                $storedItem->owner_code = $storedItem->info->owner->code;
                 $storedItem->unsetRelation('tripHistory');
+                $storedItem->info->unsetRelation('owner');
                 return $storedItem;
             });
     }
